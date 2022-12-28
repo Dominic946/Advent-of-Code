2a34377d40cbc35a54cdd805171b4743a4c0a764
Dominic Deiman <dominicdeiman@My-macbook.local>
2022-12-27 18:34:00 -0600

    Added doctumentation to utils.cc

--- 2022/src/utils/utils.cc
+++ 2022/src/utils/utils.cc
@@ -4,6 +4,27 @@
 #include <iostream>
 using namespace std;
 
+/*
+ By Dominic Deiman
+
+ My own functions that I regularly use when solving Advent of Code problems.
+ All my C++ Advent of Code solutions probably #include this file.
+*/
+
+/*
+  split(string, char)
+  Returns a vector of strings
+
+  Splits a string into a vector of strings, splitting at each instance of char.
+
+  This is probably a built in function somewhere in C++, but I couldn't find it.
+  And I know it is a built in function in other languages, but I wanted to write
+  it myself for practice anyway.
+
+  Example: if the string grocery_list is "bread-milk-egg-pie",
+           calling split(grocery_list, '-');
+           will return a string vector containing {"bread", "milk", "egg", "pie"}
+*/
 vector<string> split(string text, char splitter){
   vector<string> result;
   string temp = "";
@@ -19,6 +40,18 @@ vector<string> split(string text, char splitter){
   return result;
 }
 
+
+/*
+  split(string)
+  Returns a vector of one character long strings
+
+  Splits a string into a vector of strings,
+  where each character of the string is its own element of the vector.
+
+  Example: if the string grocery is "bread",
+           calling split(grocery);
+           will return a string vector containing {"b", "r", "e", "a", "d"}
+*/
 vector<string> split(string text){
   string temp;
   vector<string> result;
@@ -29,6 +62,15 @@ vector<string> split(string text){
   return result;
 }
 
+/*
+  printVector(vector)
+  Returns nothing
+
+  Prints every element in a vector, in a readable way.
+  Example: if you have a string vector grocery_list that contains {"bread", "milk", "egg", "pie"}
+           calling printVector(grocery_list);
+           will print to the terminal: [bread, milk, egg, pie]
+*/
 template<typename T>
 void printVector(vector<T> vec){
   cout << "[";
@@ -41,6 +83,15 @@ void printVector(vector<T> vec){
   cout << "]\n";
 }
 
+/*
+  printPairVector(vector)
+  Returns nothing
+
+  Prints every element in a vector in a readable way, where the vector contains pairs.
+  Example: if you have a vector< pair<int, int> > called coordinates that contains {(0,0), (1,2), (3,4)}
+           calling printPairVector(coordinates);
+           will print to the terminal: [<0, 0>, <1, 2>, <3, 4>]
+*/
 template<typename T>
 void printPairVector(vector< pair<T, T> > vec){
   cout << "[";
@@ -57,6 +108,15 @@ void printPairVector(vector< pair<T, T> > vec){
   cout << "]\n";
 }
 
+/*
+  vectorStoi(vector of strings)
+  Returns a vector of ints
+
+  Calls the stoi function on all strings in a vector.
+  Example: if you have a vector of strings that contains {"31", "41", "592", "65"}
+           calling vectorStoi(numbers);
+           will return a vector of integers containing: {31, 41, 592, 65}
+*/
 vector<int> vectorStoi(vector<string> vec){
   vector<int> result;
   for(int i = 0; i < vec.size(); i++){
