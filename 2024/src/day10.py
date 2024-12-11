588c887b4c9cdb13e21f6595a9ae6d08e71a9e0d
Dominic Deiman <dominic.deiman@outlook.com>
2024-12-10 22:22:56 -0600

    Completed day 10

--- 2024/src/day10.py
+++ 2024/src/day10.py
@@ -3,9 +3,27 @@ with open('../inputs/day10.txt', 'r') as file:
     input = file.readlines()
     input = [i.strip() for i in input]
 
-for i in input:
-    print(i)
+height = len(input)
+width = len(input[0])
 
-total = 0
-print(f"Part 1 answer: {total}")
-print(f"Part 2 answer: {total}")
+# Easy part 2 today, literally the only difference between part 1 and 2
+# is storing my results in a set vs a list to include/remove duplicate paths
+part1 = set()
+part2 = []
+# Basic recursive dfs, count the number of times we reach 9 by storing them in a set/list
+def ascend(y, x, origin):
+    if int(input[y][x]) == 9:
+        part1.add((origin, (y, x)))
+        part2.append((origin, (y, x)))
+    for dy, dx in [(0,1), (0,-1), (1,0), (-1,0)]:
+        if 0 <= (x + dx) < width and 0 <= (y + dy) < height:
+            if int(input[y + dy][x + dx]) == int(input[y][x]) + 1:
+                ascend(y + dy, x + dx, origin)
+
+for y, row in enumerate(input):
+    for x, elevation in enumerate(row):
+        if int(elevation) == 0:
+            ascend(y, x, (y, x))
+
+print(f"Part 1 answer: {len(part1)}")
+print(f"Part 2 answer: {len(part2)}")
