4654383e923a24f060c7ac353865f1829b81dcb6
Dominic Deiman <dominic.deiman@outlook.com>
2024-12-12 17:38:50 -0600

    Completed day 11

--- 2024/src/day11.py
+++ 2024/src/day11.py
@@ -1,11 +1,23 @@
 #!/usr/bin/python3
 with open('../inputs/day11.txt', 'r') as file:
     input = file.readlines()
-    input = [i.strip() for i in input]
+    input = [int(i.strip()) for i in input[0].split()]
 
-for i in input:
-    print(i)
+memory = dict()
+def split(rock, remaining_blinks):
+    if remaining_blinks == 0:
+        return 1
+    if (rock, remaining_blinks) not in memory:
+        num_length = len(str(rock))
+        if rock == 0:
+            total_rocks = split(1, remaining_blinks - 1)
+        elif num_length % 2 == 0:
+            total_rocks = split(int(str(rock)[:num_length // 2]), remaining_blinks - 1) + split(int(str(rock)[num_length // 2:]), remaining_blinks - 1)
+        else:
+            total_rocks = split(rock * 2024, remaining_blinks - 1)
+        memory[(rock, remaining_blinks)] = total_rocks
+    return memory[(rock, remaining_blinks)]
 
-total = 0
-print(f"Part 1 answer: {total}")
-print(f"Part 2 answer: {total}")
+
+print(f"Part 1 answer: {sum([split(i, 25) for i in input])}")
+print(f"Part 2 answer: {sum([split(i, 75) for i in input])}")
