06f0ec235d4eb8aa610e54d4170223f24abea3e0
Dominic Deiman <dominic.deiman@outlook.com>
2024-12-15 20:11:55 -0600

    Day 13 only part 1

--- 2024/src/day13.py
+++ 2024/src/day13.py
@@ -6,7 +6,7 @@ with open('../inputs/day13.txt', 'r') as file:
 p2 = 10000000000000
 
 def calculate(a, b, prize):
-    for num_b in range(1000000000000):
+    for num_b in range(100):
         if num_b * b[0] > prize[0]:
             return 0
         if (prize[0] - (b[0] * num_b)) % a[0] == 0:
@@ -22,8 +22,8 @@ for i in range(0,len(input), 4):
     A = (int(input[i].split('+')[1].split(',')[0]), int(input[i].split('+')[2]))
     B = (int(input[i+1].split('+')[1].split(',')[0]), int(input[i+1].split('+')[2]))
     prize = (int(input[i+2].split('=')[1].split(',')[0]), int(input[i+2].split('=')[2]))
-    #total += calculate(A, B, prize)
-    total2 += calculate(A, B, (prize[0] + p2, prize[1] + p2))
+    total += calculate(A, B, prize)
+    #total2 += calculate(A, B, (prize[0] + p2, prize[1] + p2))
 
 print(f"Part 1 answer: {total}")
 print(f"Part 2 answer: {total2}")
