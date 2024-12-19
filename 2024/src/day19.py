c45d3593db66de9edc73d3721e8e60fd98158a5e
Dominic Deiman <dominic.deiman@outlook.com>
2024-12-19 00:32:03 -0600

    Completed day 19

--- 2024/src/day19.py
+++ 2024/src/day19.py
@@ -6,41 +6,40 @@ with open('../inputs/day19.txt', 'r') as file:
 patterns = set(input[0].split(', '))
 memory = dict()
 
-# Recursive function to determine how many ways the given line can be made
-# It checks for all patterns which match the start of the line
+# Recursive function to determine how many ways the given design can be made.
+# It looks for for all patterns which match the start of the design, and
+# removes them, then count the arrengements in the remainder.
 
-# For example if the available patterns are [abc, a, b] and the line is "abcde"
-# Then the candidates of possible remainders are [de, bcde] because the
-# patterns 'abc' and 'a' matched, and were removed from the start of the line.
+# For example if the available patterns are [abc, a, b] and the design is "abcde"
+# then the candidates of possible remainders are [de, bcde] because the
+# patterns 'abc' and 'a' matched, and were removed from the start of the design.
 
-# So this check function is then recursively called on 'de' and 'bcde'. It goes until
+# So this count function is then recursively called on 'de' and 'bcde'. It goes until
 # either there are no possible candidates (which means the pattern can't be made)
-# or the line is empty, meaning we made the full line with patterns (success)
+# or the design is empty, meaning we made the full design with patterns (success)
 
 # I do some memoization of every successful count, to speed up the runtime significantly.
-def check(line):
-    if not line:
+def count_arrangements(design):
+    if not design:
         return 1
-    if line in memory:
-        return memory[line]
+    if design in memory:
+        return memory[design]
 
-    candidates = [line[len(p):] for p in patterns if p == line[:len(p)]]
+    candidates = [design[len(p):] for p in patterns if p == design[:len(p)]]
 
     total = 0
     for c in candidates:
-        t = check(c)
-        total += t
-    memory[line] = total
+        total += count_arrangements(c)
+    memory[design] = total
     return total
     
 total1 = 0
 total2 = 0
 for i in input[2:]:
-    print(i)
-    # check() returns a count of how many possibilities there are.
+    # count_arrangements(i) returns a count of how many possibilities there are.
     # The only difference between part 1 and part 2 is that 1 only wants to know
-    # how many times that count is > 0, while part 2 sums the counts themselves.
-    result = check(i)
+    # how many times that result is > 0, while part 2 sums the counts themselves.
+    result = count_arrangements(i)
     total1 += bool(result)
     total2 += result
 
